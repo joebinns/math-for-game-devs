4792fe3ac153bc76c76cff22201de00c305add50
Joe Binns <joebinns.95@gmail.com>
2022-11-05 10:18:26 +0100

    Adjust camera to top down

--- Math for Game Devs/Assets/Scripts/Camera/CameraRotator.cs
+++ Math for Game Devs/Assets/Scripts/Camera/CameraRotator.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,10 +9,12 @@ public class CameraRotator : MonoBehaviour
     private Vector2 _cursorPosition = Vector3.zero;
     private Vector2 _cursorDelta;
     private Transform _cameraPivot;
+    private Quaternion _initialRotation;
 
     private void Awake()
     {
         _cameraPivot = this.transform;
+        _initialRotation = _cameraPivot.rotation;
         CursorMode.DisableCursor();
     }
     
@@ -24,11 +27,11 @@ public class CameraRotator : MonoBehaviour
     {
         _cursorPosition += _cursorDelta * _sensitivity;
         _cursorPosition.y = Mathf.Clamp(_cursorPosition.y, -60f, 60f);
-        
-        var rotation = _cameraPivot.rotation.eulerAngles;
-        rotation.x = - _cursorPosition.y;
-        rotation.y = _cursorPosition.x;
 
+        var rotation = _initialRotation.eulerAngles;
+        rotation.x += - _cursorPosition.y;
+        rotation.y += _cursorPosition.x;
+        
         _cameraPivot.rotation = Quaternion.Euler(rotation);
     }
 }
